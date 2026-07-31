57fe1be9992325db4f5c233e1fc478014b349cf3
MJ Rossetti <s2t2@users.noreply.github.com>
2026-07-30 20:43:11 -0400

    Added tests
    and fixed a bug in the RPS game winner determination logic

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+# this is the "app/__init__.py" file
+# it can be empty
+# it helps make our local imports work
--- test/stocks_test.py
+++ test/stocks_test.py
@@ -1,4 +1,23 @@
 
 # this is the "test/stocks_test.py" file...
 
-# todo: test the functionality in the "app/stocks.py" file
+from pandas import DataFrame
+
+from app.stocks import fetch_stocks_csv
+
+
+def test_data_fetching():
+
+    stocks_df = fetch_stocks_csv("GOOGL")
+
+    assert isinstance(stocks_df, DataFrame)
+
+    assert "timestamp" in stocks_df.columns
+    assert "adjusted_close" in stocks_df.columns
+
+    assert len(stocks_df) >= 100
+
+    # ideally we would test specific values
+    # but this data gets refreshed every day
+    # so that's why we're just kind of testing its structure (cols)
+    # assert stocks_df["adjusted_close"][0] == 336.71
